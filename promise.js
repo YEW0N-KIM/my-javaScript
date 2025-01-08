dca3e624e004551d71d139b6107d18528dde859f
yewon_kim <wonwhimsical@gmail.com>
2025-01-08 21:02:14 +0900

    study : promise

---
+++ promise.js
@@ -0,0 +1,10 @@
+const myPromise = new Promise(() => {
+    
+    // 비동기 작업 처리
+    setTimeout(()=>{
+        const text =prompt("Hello를 입력해줘! 그러면 선물을 줄께!")
+        if(text ==='Hello'){
+            
+        }
+    },2000)
+})
