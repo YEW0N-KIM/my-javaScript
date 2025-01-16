43f167b12090d2963b67fc3f23ad11c4375d14af
yewon_kim <wonwhimsical@gmail.com>
2025-01-16 21:04:15 +0900

    modify: sidebar

---
+++ sidebar/sidebar.js
@@ -0,0 +1,11 @@
+let mainList = document.querySelectorAll('.level-1');
+
+// mainmenu click event
+mainList.forEach((main)=>{
+    main.addEventListener('click', function(){
+        main.classList.add('active');
+
+        let sub = main.closest('.level-2');
+        console.log(sub);
+    });
+})
